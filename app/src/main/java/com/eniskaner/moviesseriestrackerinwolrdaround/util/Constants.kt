d88f27e566e5784187e85367cede565617d68f0d
Enis Kaner <enis.d.kaner@gmail.com>
2023-08-24 13:12:03 +0300

    Constants and Resource added on util

---
+++ app/src/main/java/com/eniskaner/moviesseriestrackerinwolrdaround/util/Constants.kt
@@ -0,0 +1,13 @@
+package com.eniskaner.moviesseriestrackerinwolrdaround.util
+
+object Constants {
+
+    const val MOVIE_ID = 1234
+    const val SERIES_ID = 4321
+    const val MOVIEDB_URL="https://api.themoviedb.org"
+    const val MOVIEDB_KEY="Replace with your api key"
+    const val POSTER_URL = "https://image.tmdb.org/t/p/w342"
+    const val LOGO_URL = "https://image.tmdb.org/t/p/w500"
+    const val BACKDROP_URL = "https://image.tmdb.org/t/p/w780"
+    const val RANDOM_POSTER = "/a6ptrTUH1c5OdWanjyYtAkOuYD0.jpg"
+}
